6f7f09c3b73047efe0ca5ff6471554f758c94137
Hugh Brien <hugh@hughbrien.com>
2022-07-27 15:55:03 -0400

    Update gremlin-agent-install.sh

--- gremlin-agent-install.sh
+++ gremlin-agent-install.sh
@@ -8,7 +8,7 @@ helm install gremlin gremlin/gremlin --namespace gremlin \
     --set gremlin.container.driver=any \
     --set gremlin.secret.managed=true \
     --set gremlin.secret.type=secret \
-    --set gremlin.secret.teamID=a4aa6238-c6ed-470f-aa62-38c6ed770ff7   \
-    --set gremlin.secret.clusterID=hughbrien-bank-of-athos \
-    --set gremlin.secret.teamSecret=ca1c657c-154f-4456-9c65-7c154fb45646
+    --set gremlin.secret.teamID=<teamid>   \
+    --set gremlin.secret.clusterID=cluster-bank-of-anthos \
+    --set gremlin.secret.teamSecret=<secret>
 
