1947d296329974c73993fcd88fc8c62e8cf438cd
ubuntu <hugh.brien@gremlin.com>
2022-08-12 20:09:55 +0000

    Updated frontend

--- src/frontend/push-frontend-container.sh
+++ src/frontend/push-frontend-container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export VERSION=1.1.7
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/z6u6t6j2
+export VERSION=1.1.8
+#aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/z6u6t6j2
 
-docker build . --platform linux/amd64  --tag boa-frontend:${VERSION}
-docker tag boa-frontend:${VERSION} public.ecr.aws/z6u6t6j2/boa-frontend:${VERSION}
-docker push public.ecr.aws/z6u6t6j2/boa-frontend:${VERSION}
+sudo docker build . --tag frontend:${VERSION}
+sudo docker tag boa-frontend:${VERSION} public.ecr.aws/z6u6t6j2/frontend:${VERSION}
+sudo docker push public.ecr.aws/z6u6t6j2/frontend:${VERSION}
 
 
 
